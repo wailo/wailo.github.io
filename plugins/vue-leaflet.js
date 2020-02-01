8776178cf14591dc80a7e427cb8d02c621992fcc
WY <wailo_2@hotmail.com>
2020-02-01 13:12:57 -0300

    Fixed dependencies and added pushdir plugin to accelerate deployment

--- plugins/vue-leaflet.js
+++ plugins/vue-leaflet.js
@@ -4,12 +4,6 @@ import * as Vue2Leaflet from 'vue2-leaflet'
 import L from 'leaflet'
 delete L.Icon.Default.prototype._getIconUrl
 
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
-})
-
 Vue.component('l-map', Vue2Leaflet.LMap)
 Vue.component('l-tilelayer', Vue2Leaflet.LTileLayer)
 Vue.component('l-marker', Vue2Leaflet.LMarker)
