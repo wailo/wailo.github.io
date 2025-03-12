b15e0a9d317f6be374c03704e52d9e1020bd004b
wy <wy@WYs-MacBook-Pro.local>
2025-03-12 15:17:47 +0800

    Update Session ID input

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="relative h-screen flex items-center justify-center bg-custom-dark text-custom-gray overflow-hidden">
-        
+
       <!-- Content -->
     <div class="relative text-center px-6">
       <h1 class="text-5xl md:text-7xl font-extrabold uppercase tracking-wide relative title-container">
@@ -19,8 +19,16 @@
 
       <!-- Launch -->
       <div class="flex flex-col justify-center items-center border-gray-500 py-2 mt-4">
-    <input class="appearance-none bg-transparent border-2 border-custom-gray rounded w-[45%] text-center mr-3 py-1 px-2 leading-tight focus:outline-none" v-model="roomId" type="text" :placeholder="`Type Room ID`">
-    <router-link :to="`/sim?roomId=${roomId || randomRoomId}`">
+
+  <input
+      class="appearance-none bg-transparent border-2 border-custom-gray rounded w-[45%] text-center mr-3 py-1 px-2 leading-tight focus:outline-none"
+      type="text"
+      :placeholder="`Session ID: ${roomId}`"
+      :value="isFocused ? roomId : ''"
+      @focus="isFocused = true"
+      @blur="isFocused = false"
+      @input="roomId = ($event.target as HTMLInputElement).value">
+    <router-link :to="`/sim?roomId=${roomId}`">
     <button class="flex-shrink-0 bg-red-500 hover:bg-red-700 border-red-500 hover:border-red-700 text-sm border-2 text-white py-1 mt-2 px-2 rounded" type="button">
       Launch
     </button>
@@ -32,21 +40,20 @@
    </div>
 
   <!-- <HomeAnimation class="bg-custom-dark"/>  -->
-  
+
   </template>
-  
+
   <script setup lang="ts">
   import { ref, computed } from 'vue';
   // import HomeAnimation from './HomeAnimation.vue';
 
   const title = ref("Flight Simulator");
   const sentence = ref("Minimal Versatile Intellegent");
-  const randomRoomId = computed(() => Math.floor(100000 + Math.random() * 10000).toString());
-  const roomId = randomRoomId;
-  
+  const roomId = ref(Math.floor(100000 + Math.random() * 10000).toString());
+  const isFocused = ref(false);
   const words = computed(() => sentence.value.split(" "));
   </script>
-  
+
   <style scoped>
   /* Custom Colors */
   .bg-custom-dark {
@@ -57,10 +64,10 @@
   }
 
 
-  
 
 
-  
+
+
   /* Fade-in animation for text */
   @keyframes fade-in {
     from {
@@ -72,7 +79,7 @@
       transform: translateY(0);
     }
   }
-  
+
   .animate-fade-in {
     animation: fade-in 0.8s ease-in-out forwards;
     display: inline-block;
