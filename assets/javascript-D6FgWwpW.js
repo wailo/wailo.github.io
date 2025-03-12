6bdb1d9fa5d1c778e604e2919a0e8f0d2f0325f7
wailo <wailo_2@hotmail.com>
2025-03-12 07:28:36 +0000

    Deploying to master from @ wailo/wailo.github.io@b15e0a9d317f6be374c03704e52d9e1020bd004b 🚀

--- assets/javascript-D6FgWwpW.js
+++ assets/javascript-D6FgWwpW.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-DWzfbeg9.js";import"./index-93NpQGT9.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-BtpAnPcB.js";import"./index-CqsijQGt.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.33.0(4b1abad427e58dbedc1215d99a0902ffc885fcd4)
  * Released under the MIT license
