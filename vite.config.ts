eec4c4373b007892a83e5fcee57c9cf643d0b72b
wy <wy@WYs-MacBook-Pro.local>
2025-04-16 15:31:15 +0800

    Add checker for development environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), checker({
+    // e.g. use TypeScript check
+    typescript: true,
+  })],
   server: {
     // Work around for peerJs connection in dev setup
     host: "127.0.0.1",
