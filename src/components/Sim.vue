wailo/wailo.github.io

commit 8f8cc42fbfc3a73fc1e593151ff963168596b953

Add version metadata

src/components/Sim.vue
@@ -305,6 +305,7 @@ onMounted(async () => {
   })
     .then((module) => {
       FlightSimModule = module;
+      sim_data = reactive(FlightSimModule.simData)
       autopilotProps = computed(() =>
         getAutopilotProperties(FlightSimModule, sim_data),
       );

src/siminterfac.ts
@@ -4,6 +4,7 @@ let FlapSelectorKeys: { [key: number]: string } = {};
 let GearSelectorKeys: { [key: number]: string } = {};
 
 export class SimData {
+  api_version: string = "";
   api_fps: number = 0;
   api_ups: number = 0;
   api_simulation_speed: number = 0;
@@ -77,6 +78,7 @@ export type SimulationDataDisplay = {
 
 
 export const simulationDataDisplay:  SimulationDataDisplay = {
+  api_version : {api: "api_version", label: "Flight Model Version", visible: false},
   api_fps: { api: "api_fps", label: "Frames Per Second", visible: false },
   api_ups: { api: "api_ups", label: "Update Per Second", visible: false },
   api_simulation_speed: { api: "api_simulation_speed", label: "Simulation Speed", visible: false },
@@ -321,6 +323,7 @@ export async function fetchSimData(module: MainModule, payload: SimData) {
   }
   // Detect sim reset and update memory addresses
   if (ptrApiWeight !== module._api_weight() >> 2) {
+    payload.api_version = module.VERSION_STRING.toString();
     init(module);
   }
 

commit 4d89eba64aa24f1df98ef164ee8a707bba6280d8

Remove a bug that was introduced in 8f8cc42

src/components/Sim.vue
@@ -388,7 +388,7 @@ onMounted(async () => {
     .then((module) => {
       FlightSimModule = module;
       FlightSimModule.GLFW.requestFullscreen = toggleFullscreen; // Replace with custom implementation
-      sim_data = reactive(FlightSimModule.simData)
+
       autopilotControlsButtons = computed(() =>
         getAutopilotProperties(FlightSimModule, sim_data).filter(item => item.inputValue === undefined),
       );