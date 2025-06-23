fc77aaf5eb8b8a20f236d15f52e9994078d7ff79
wailo <wailo_2@hotmail.com>
2025-06-23 16:24:14 +0000

    Deploying to master from @ wailo/wailo.github.io@bc6abdf5e4466bc028d0fa9435653d1bebae680d 🚀

--- assets/javascript-DQCZeFi1.js
+++ assets/javascript-DQCZeFi1.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-KD3JTM4u.js";import"./index-CNZCQPbP.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-Cfeo7918.js";import"./index-CsODTUcU.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.33.0(4b1abad427e58dbedc1215d99a0902ffc885fcd4)
  * Released under the MIT license
