94f32cc085716253648411145d87af6bedf425c2
wailo <wailo_2@hotmail.com>
2025-07-01 09:53:58 +0000

    Deploying to master from @ wailo/wailo.github.io@417db79831428883f4f8400c09305f827b14e316 🚀

--- assets/javascript-Cubctrda.js
+++ assets/javascript-Cubctrda.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-CGexSvz6.js";import"./index-CFVavFB2.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-C4B1CWNV.js";import"./index-Gir8o7Qc.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.33.0(4b1abad427e58dbedc1215d99a0902ffc885fcd4)
  * Released under the MIT license
