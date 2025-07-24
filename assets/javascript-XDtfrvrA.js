e3149580a1b5a8fc873bca0bc07b9ab4a5692e1d
wailo <wailo_2@hotmail.com>
2025-07-24 17:58:23 +0000

    Deploying to master from @ wailo/wailo.github.io@d5f69e721c8098232ff0cb45e00d9486afaa8e42 🚀

--- assets/javascript-XDtfrvrA.js
+++ assets/javascript-XDtfrvrA.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-BTmVBgXe.js";import"./index-CKEaUoP9.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-Cx1fS2QZ.js";import"./index-DlXpIRGj.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.33.0(4b1abad427e58dbedc1215d99a0902ffc885fcd4)
  * Released under the MIT license
