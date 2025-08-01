d5914fc3ca54bccf8f395dd31b106dafaf110312
wailo <wailo_2@hotmail.com>
2025-08-01 01:05:09 +0000

    Deploying to master from @ wailo/wailo.github.io@3b580848ab225f62aec01ee8624718b7355618cb 🚀

--- assets/html-CYRGSMVT.js
+++ assets/html-CYRGSMVT.js
@@ -1,4 +1,4 @@
-import{m as a}from"./index-K1wYkqpb.js";/*!-----------------------------------------------------------------------------
+import{m as a}from"./index-BzTI_Jcr.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.33.0(4b1abad427e58dbedc1215d99a0902ffc885fcd4)
  * Released under the MIT license
