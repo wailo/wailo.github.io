afc4b5ffc69d4bf0d0dce0169dc8f62afff61c39
wailo <wailo_2@hotmail.com>
2026-02-09 09:09:17 +0000

    Deploying to master from @ wailo/wailo.github.io@38ebce0ca925842f1623676b7fbd2b0828036acc 🚀

--- assets/javascript-C-GtvZy6.js
+++ assets/javascript-C-GtvZy6.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-DfJDUP20.js";import"./index-COqOR5zt.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-dlS9O5IN.js";import"./index-wE7Kdy-9.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.33.0(4b1abad427e58dbedc1215d99a0902ffc885fcd4)
  * Released under the MIT license
