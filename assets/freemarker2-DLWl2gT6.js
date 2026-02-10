87920d59f5e5551453110a60d867bba379272df4
wailo <wailo_2@hotmail.com>
2026-02-10 07:35:16 +0000

    Deploying to master from @ wailo/wailo.github.io@6389c026699af7a3041571ac4d6d7ee094a4ae7e 🚀

--- assets/freemarker2-DLWl2gT6.js
+++ assets/freemarker2-DLWl2gT6.js
@@ -1,4 +1,4 @@
-import{m as f}from"./index-mkjAaRrE.js";/*!-----------------------------------------------------------------------------
+import{m as f}from"./index-CmmGToNe.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.33.0(4b1abad427e58dbedc1215d99a0902ffc885fcd4)
  * Released under the MIT license
--- assets/javascript-zUsBTpwR.js
+++ assets/javascript-zUsBTpwR.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-DguFYUkK.js";import"./index-mkjAaRrE.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-CnEygbkH.js";import"./index-CmmGToNe.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.33.0(4b1abad427e58dbedc1215d99a0902ffc885fcd4)
  * Released under the MIT license
