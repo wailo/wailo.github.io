4a7101c9a843e26d39f458c4364fb221da7512b2
wailo <wailo_2@hotmail.com>
2026-05-07 22:27:55 +0000

    Deploying to master from @ wailo/wailo.github.io@1628bce4425b18fa768f09dfc0ce51086cfb638e 🚀

---
+++ LearningModules/phugoid.ts
@@ -0,0 +1,77 @@
+import { ScriptContext } from "../../src/core";
+
+export async function main(context: ScriptContext) {
+  const simControls = context.controls;
+  const simProps = context.props;
+  const repositionWithAutopilot = context.repositionWithAutopilot;
+  const waitFor = context.waitFor;
+  // const waitForCondition = context.waitForCondition;
+  // const dataView = context.dataView;
+  const plotView = context.plotView;
+  const dataDisplayReset = context.dataDisplayReset;
+  const notifyUser = context.notifyUser;
+  const checkPoint = context.checkPoint;
+
+
+// Demostrate phugoid mode
+notifyUser("Phugiod Mode", "Observe the aircraft's oscillation in pitch and altitude.\n\n" + 
+    "Lesson plan:\n\n" +
+    "1. Reposition to safe alitude and speed. Aircraft is trimmed, autopilot disengaged\n" +
+    "2. Apply stick input for 2 seconds to disturb altitude and speed. then return to normal\n" +
+    "3. Observe the aircraft's oscillation in pitch and altitude.\n" + 
+    "4. Increase the simulation speed to observe the damping effect\n"
+);
+
+const flightModel = simControls.simulation.set_flight_model_b747();
+
+// Pre-configuration before trimming the aircraft.
+const preConfiguration = () => {
+    simControls.flightModel.set_flaps_selector_position(simControls.B747FlapSelector.TWENTY);
+}
+
+// simControls.simulation.reset_simulation();
+dataDisplayReset();
+
+await repositionWithAutopilot(context, 3000, 180, 0, 10000, preConfiguration);
+// await waitFor(1000)
+plotView(simProps.elevator_position, true);
+plotView(simProps.pitch, true);
+plotView(simProps.pitch_dot, true);
+plotView(simProps.speed_IAS, true);
+await waitFor(5000);
+checkPoint("Initiating Phugiod mode")
+notifyUser("Initiating Phugiod mode", "Pulling the control stick for 2 seconds then return to neutral position.\n\n")
+// Show motions cues
+simControls.simulation.set_motion_cues(true)
+await waitFor(5000)
+flightModel.set_elevator_position(flightModel.elevator_position - 0.50)
+await waitFor(2000)
+flightModel.set_elevator_position(0.00)
+await waitFor(5000)
+checkPoint("Phugiod mode")
+notifyUser("Phugiod mode", "Observe how the aircraft oscillates, Observe change in pitch angle with the speed\n\n")
+await waitFor(30000)
+notifyUser("Phugiod mode", "We will increase the simulation speed gradually.\n\n" + 
+    "This will help you observe oscillation and the damping effect of the phugoid mode.\n\n")
+await waitFor(3000)
+simControls.simulation.set_simulation_speed(simControls.simulation.simulation_speed +1);
+await waitFor(2000)
+simControls.simulation.set_simulation_speed(simControls.simulation.simulation_speed +2);
+await waitFor(2000)
+simControls.simulation.set_simulation_speed(simControls.simulation.simulation_speed +2);
+await waitFor(2000)
+simControls.simulation.set_simulation_speed(simControls.simulation.simulation_speed +2);
+await waitFor(2000)
+simControls.simulation.set_simulation_speed(simControls.simulation.simulation_speed +2);
+notifyUser("Phugiod mode", `The simulation speed is now ${simControls.simulation.simulation_speed}x.\n\n` +
+    "Observe the aircraft's oscillation in pitch and altitude.\n\n" +
+    "The oscillation is damped, meaning the amplitude of the oscillation decreases over time.\n\n" +
+    "This is a characteristic of the phugoid mode.")
+await waitFor(25000)
+notifyUser("Phugiod mode", "By now the Phugiod mode oscillation is almost damped.\n\n" +
+    "this mark the end of the phugoid mode demonstration.\n\n")
+checkPoint("End of Phugiod mode")
+await waitFor(10000)
+simControls.simulation.set_simulation_speed(1);
+// simControls.flightModel.set_autopilot(true);
+}
