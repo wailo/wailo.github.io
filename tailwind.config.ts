50b538d32f0b7edf6e7aad53de69917e2eec4498
wy <wy@wy>
2026-05-24 16:32:13 +0100

    CSS: Update CSS variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,37 +1,37 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {
       fontSize: {
-        panelFont: "0.65rem",
+        panelFont: '0.65rem',
       },
       fontFamily: {
-        panelFont: ["Orbitron", "Courier New", "Courier", "monospace"],
+        panelFont: ['Orbitron', 'Courier New', 'Courier', 'monospace'],
       },
       fontWeight: {
-        light: "300",
-        medium: "500",
-        bold: "700",
-        black: "900",
+        light: '300',
+        medium: '500',
+        bold: '700',
+        black: '900',
       },
       colors: {
-        primary: "rgb(var(--color-primary) / <alpha-value>)",
-        secondary: "rgb(var(--color-secondary) / <alpha-value>)",
-        openglCanvasBackground: "#00000000",
-        panelBorder: "rgb(var(--color-panelBorder) / <alpha-value>)",
-        panelHeaderBackground: "rgb(var(--color-panelHeaderBackground) / <alpha-value>)",
-        panelStatusBackground: "rgb(var(--color-panelStatusBackground) / <alpha-value>)",
-        panelContentBackground: "rgb(var(--color-panelContentBackground) / <alpha-value>)",
-        simElementBorder: "rgb(var(--color-simElementBorder) / <alpha-value>)",
-        simInputBackground: "rgb(var(--color-simInputBackground) / <alpha-value>)",
-        panelActive: "#b9391b",
-        simBackground: "rgb(var(--color-simBackground) / <alpha-value>)",
-        simActiveButton: "rgb(var(--color-simActiveButton) / <alpha-value>)",
+        primary: 'rgb(var(--color-primary) / <alpha-value>)',
+        secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+        openglCanvasBackground: '#00000000',
+        panelBorder: 'rgb(var(--color-panelBorder) / <alpha-value>)',
+        panelHeaderBackground: 'rgb(var(--color-panelHeaderBackground) / <alpha-value>)',
+        panelStatusBackground: 'rgb(var(--color-panelStatusBackground) / <alpha-value>)',
+        panelContentBackground: 'rgb(var(--color-panelContentBackground) / <alpha-value>)',
+        simElementBorder: 'rgb(var(--color-simElementBorder) / <alpha-value>)',
+        simInputBackground: 'rgb(var(--color-simInputBackground) / <alpha-value>)',
+        panelActive: 'rgb(var(--color-panelActive) / <alpha-value>)',
+        simBackground: 'rgb(var(--color-simBackground) / <alpha-value>)',
+        simActiveButton: 'rgb(var(--color-simActiveButton) / <alpha-value>)',
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
